75275f5415bcec088d12ea3174098e1a4e0a0f46
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2020-07-03 17:33:29 -0300

    Mapeando relacionamento entre entidades Product e Category

--- src/main/java/com/org/springboot/productsCategory/domain/Product.java
+++ src/main/java/com/org/springboot/productsCategory/domain/Product.java
@@ -2,14 +2,26 @@ package com.org.springboot.productsCategory.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Product implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
 	
+	@ManyToOne
+	@JoinColumn(name = "category_id") // chave estrangeira
 	private Category category;
 	
 	public Product() {
