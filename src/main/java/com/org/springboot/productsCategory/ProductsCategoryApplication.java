5d6950813f12f64cd3fd132ed14b468b687d984d
Tatiane Takeuti <tatiane.takeuti@hotmail.com>
2020-07-03 15:42:24 -0300

    Projeto criado com spring initializr

---
+++ src/main/java/com/org/springboot/productsCategory/ProductsCategoryApplication.java
@@ -0,0 +1,13 @@
+package com.org.springboot.productsCategory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsCategoryApplication.class, args);
+	}
+
+}
